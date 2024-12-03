66a72635b6ab0fea7ed2b4dea95ca7780736eb02
Pavanaprana Dasa <pavamana1123@gmail.com>
2024-12-03 20:21:31 +0530

    buttons

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 const moment = require("moment")
 const consolas = console.log
 console.log = function (...args) {
-  const timestamp = new Date().toISOString()
-  consolas.apply(console, [`[${moment(timestamp).format("YY-MMM-DD HH:mm")}]`, ...args])
+    const timestamp = new Date().toISOString()
+    consolas.apply(console, [`[${moment(timestamp).format("YY-MMM-DD HH:mm")}]`, ...args])
 }
 
 const express = require('express')
 const axios = require('axios')
 
 const app = express()
-app.use(express.json()) 
+app.use(express.json())
 const port = 4004
 
 var cred = require("./cred.js")
 
 const TelegramBot = require('node-telegram-bot-api')
-const bot = new TelegramBot(cred.telebot.botToken, {polling: false})
+const bot = new TelegramBot(cred.telebot.botToken, { polling: false })
 
-app.post('/', (req, res)=>{
+app.post('/', (req, res) => {
     const { body } = req
 
-        var reqAPIKey = req.get("api-key")
-        if(cred.apiKey == reqAPIKey){
-            const { template, phone, headers, values, buttons } = body
-            axios.post(cred.wame.url, 
-            {
-                countryCode: "+91",
-                phoneNumber: phone,
-                type: "Template",
-                template: {
-                    name: template,
-                    languageCode: "en",
-                    headerValues: headers || [],
-                    bodyValues: values || [],
-                    buttonValues: buttons || {} 
-                }
-            },
+    var reqAPIKey = req.get("api-key")
+    if (cred.apiKey == reqAPIKey) {
+        const { template, phone, headers, values, buttons } = body
+
+        const payload = {
+            countryCode: "+91",
+            phoneNumber: phone,
+            type: "Template",
+            template: {
+                name: template,
+                languageCode: "en",
+                headerValues: headers || [],
+                bodyValues: values || [],
+            }
+        }
+
+        if(buttons){
+            payload.buttonValues = buttons
+        }
+
+        axios.post(cred.wame.url, payload,
             {
                 headers: {
                     'content-type': 'text/json',
@@ -53,10 +58,10 @@ app.post('/', (req, res)=>{
                 res.status(err.response.status)
                 res.send(err.response.data.message)
             })
-        }else{
-            res.status(401).send("API Key mismatch")
-        }
-    
+    } else {
+        res.status(401).send("API Key mismatch")
+    }
+
 })
 
 app.listen(port, () => {
